a6397529d318839ba1af8811c79ad6658a6c4d39
Ludovic Auxepaules <ludovic.auxepaules@upmc.fr>
2014-12-18 15:16:07 +0100

    Update spring-beans xsd links, fix spaces, tabs, add comments 'If USE ...'

--- cas-toolbox-core/src/main/java/org/jasig/cas/services/RegRegisteredServiceImpl.java
+++ cas-toolbox-core/src/main/java/org/jasig/cas/services/RegRegisteredServiceImpl.java
@@ -1,8 +1,8 @@
 package org.jasig.cas.services;
 
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.jasig.cas.authentication.principal.Service;
 import org.jasig.cas.services.RegisteredServiceImpl;
@@ -11,18 +11,17 @@ import java.util.regex.*;
 
 public class RegRegisteredServiceImpl extends RegisteredServiceImpl {
 	private static final long serialVersionUID = -5136718302682868276L;
-	protected final Log logger = LogFactory.getLog(this.getClass());
-    
-    public boolean matches(final Service service) {
-    	
-    	if(service != null ) {
-    		Pattern p = Pattern.compile(getServiceId(),Pattern.CASE_INSENSITIVE);
-    		Matcher m = p.matcher(service.getId());
-    		boolean result = m.find();
-    		        	
-    		if(result)
-    			return true;
-    	}
-    	return false;
-    }
+	protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+	public boolean matches(final Service service) {
+		if(service != null ) {
+			Pattern p = Pattern.compile(getServiceId(),Pattern.CASE_INSENSITIVE);
+			Matcher m = p.matcher(service.getId());
+			boolean result = m.find();
+						
+			if(result)
+				return true;
+		}
+		return false;
+	}
 }
