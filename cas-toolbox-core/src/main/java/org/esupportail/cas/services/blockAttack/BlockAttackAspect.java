bf4dbe64cd11b1f2bf523722de14530d9025d96f
Ludovic Auxepaules <ludovic.auxepaules@upmc.fr>
2015-09-22 21:56:25 +0200

    Generate Licence, format Java files : replace credentials by credential in block attacks classes

--- cas-toolbox-core/src/main/java/org/esupportail/cas/services/blockAttack/BlockAttackAspect.java
+++ cas-toolbox-core/src/main/java/org/esupportail/cas/services/blockAttack/BlockAttackAspect.java
@@ -1,7 +1,20 @@
-/*
- * Copyright 2007 The JA-SIG Collaborative. All rights reserved. See license
- * distributed with this file and available online at
- * http://www.ja-sig.org/products/cas/overview/license/
+/**
+ * Licensed to Esup-Portail under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Esup-Portail licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License.  You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.esupportail.cas.services.blockAttack;
 
@@ -30,16 +43,16 @@ public final class BlockAttackAspect{
 	public void inExecute() { }	
 	
 	
-	@Around("inExecute() && args(credentials,..)")
-	public Object AroundExecute(ProceedingJoinPoint pjp, UsernamePasswordCredential credentials) throws Throwable {
+	@Around("inExecute() && args(credential,..)")
+	public Object AroundExecute(ProceedingJoinPoint pjp, UsernamePasswordCredential credential) throws Throwable {
 		if(!enabled) {
 			return pjp.proceed();
 		}
 		
-		String username = credentials.getUsername();
+		String username = credential.getUsername();
 		
 		if(accountLockingService.isAccountLocked(username)) {
-			log.error("Account \""+username+"\" is locked for : "+accountLockingService.getLockTime()/1000+" s");			
+			log.error("Account \""+username+"\" is locked for : "+accountLockingService.getLockTime()/1000+" s");
 			makeWait();
 			throw new BadCredentialsAuthenticationException(CODE);
 		}
